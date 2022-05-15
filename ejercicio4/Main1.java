0dc502d92c2d88510f0b6d2fa22589e5616df5dd
Edisson Franklin <69181980+Edisson-Acc-UNSA@users.noreply.github.com>
2022-05-15 16:43:40 -0500

    Primera implementación de la clase Main
    En esta clase iremos probando los metodos que se vayan implementando

---
+++ ejercicio4/Main1.java
@@ -0,0 +1,14 @@
+package Ejercicio4;
+
+public class Main1 {
+	public static void main(String[] args) {
+		List <Integer>lista = new List<Integer>();
+		lista.add(5);
+		lista.add(8);
+		lista.add(17);
+		lista.add(50);
+		System.out.println("Se esta ejecutando hasta aqui");
+		System.out.println("numero de elementos: " + lista.size());
+	}
+}
+
