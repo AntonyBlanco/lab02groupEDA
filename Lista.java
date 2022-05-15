fa0b416056b843bd6b4538f2564cd33b8aa88579
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-15 17:29:11 -0500

    Lista: Se implementaron add(index,element), clear() de List y se crearon 2 metodos mas para mejor manipulacion del atributo arrContent

--- Lista.java
+++ Lista.java
@@ -1,4 +1,46 @@
-public class Lista{
+import java.util.List;
 
+public class Lista {
+	private T[] arrContent;
+	private size = 0;
+
+	public Lista(){
+		arrContent = new T[2];
+	}
+
+	public void add(int index, T element){
+		if(index == size){
+			if(arrContent.length <= index)
+				makeArrContentBigger();
+			arrContent[index] = element;
+			size++;
+			return;
+		}
+		// else
+		for(int i=0){
+			
+		}
+	}
+	public void clear(){
+	// Elimina todos los elementos de la lista
+	// Establece el arreglo a uno vacío de tamaño 2
+		this.arrContent = new T[2];
+	}
+	private void makeArrContentBigger(){
+	// Incrementa el tamaño del arreglo al doble
+		T[] newArr = new T[arrContent.length * 2];
+		for(int i = 0; i < arrContent; i++){
+			newArr[i] = arrContent[i];
+		}
+	}
+	private void makeArrContentSmaller(){
+	// Decrementa el tamaño del arreglo a la mitad
+		if(arrContent.length <= 2)
+			return;
+		T[] newArr = new T[arrContent.length / 2];
+		for(int i = 0; i < newArr.length; i++){
+			newArr[i] = arrContent[i];
+		}
+	}
 }
 
