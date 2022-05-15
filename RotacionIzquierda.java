ccdd07af85717357d1c15b69feb84925a567354e
AntonyBlanco <ablancot@unsa.edu.pe>
2022-05-15 09:27:33 -0500

    Correccion en la implementacion de rotarIzquierdaArray() donde index se salía del arreglo, clase testeable

--- RotacionIzquierda.java
+++ RotacionIzquierda.java
@@ -1,6 +1,14 @@
 public class RotacionIzquierda{
 	public static void main(String[] args){
+		System.out.println(" ---- Rotar arreglo izquierda ---- ");
+		int arr[] = {1, 2, 3, 4, 5};
+
+		printArr(arr);
 		
+		int rotado[] = rotarIzquierdaArray(arr,2);
+
+		printArr(rotado);
+
 	}
 	public static int[] rotarIzquierdaArray(int[] A, int d){
 	// Metodo para rotar los elementos de un arreglo a la izquierda
@@ -9,7 +17,7 @@ public class RotacionIzquierda{
 		int index;
 		for(int i = 0; i < A.length; i++){
 			index = i + d; // Nueva posicion del elemento
-			if(index > A.length) index -= A.length; // Limitando a index
+			if(index >= A.length) index -= A.length; // Limitando a index
 			Arotated[i] = A[index];
 		}
 		return Arotated; // Retorna arreglo diferente a A
